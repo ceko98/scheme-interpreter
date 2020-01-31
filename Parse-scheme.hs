fbd5ebc4eddbee49154accde5dee2dad23bc320f
ceko98 <ceko9898@gmail.com>
2020-01-31 23:48:01 +0200

    Add prse methods file

---
+++ Parse-scheme.hs
@@ -0,0 +1,12 @@
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}     -- cover all cases!
+{-# OPTIONS_GHC -fwarn-unused-matches #-}          -- use all your pattern matches!
+{-# OPTIONS_GHC -fwarn-missing-signatures #-}      -- write all your toplevel signatures!
+{-# OPTIONS_GHC -fwarn-name-shadowing #-}          -- use different names!
+{-# OPTIONS_GHC -fwarn-incomplete-uni-patterns #-} -- warn about incomplete patterns v2
+
+import Parser
+  ( Parser, nom
+  , parse
+  , result, empty, (<|>)
+  , many, some
+  )
